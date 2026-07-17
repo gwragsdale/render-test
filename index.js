ee365d103d5e12324482cd5525f3b8c1a78b861b
Grant Ragsdale <gragsdale1988@gmail.com>
2026-07-17 16:22:20 -0500

    part 3.d

--- index.js
+++ index.js
@@ -51,19 +51,18 @@ app.get("/api/notes/:id", (request, response, next) => {
     .catch((err) => next(err));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({ error: "content missing" });
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => response.json(savedNote));
+  note
+    .save()
+    .then((savedNote) => response.json(savedNote))
+    .catch((err) => next(err));
 });
 
 app.delete("/api/notes/:id", (request, response, next) => {
@@ -98,6 +97,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
